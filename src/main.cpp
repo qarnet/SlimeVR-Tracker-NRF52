ef15f7e0671207e5c5dbcf56f79defdea5ada0b6
qarnet <thomaskirschner85@yahoo.de>
2023-12-30 13:16:31 +0100

    platformio.ini settings for xiao nrf52

--- src/main.cpp
+++ src/main.cpp
@@ -21,126 +21,127 @@
     THE SOFTWARE.
 */
 
-#include "Wire.h"
-#include "ota.h"
-#include "GlobalVars.h"
-#include "globals.h"
-#include "credentials.h"
-#include <i2cscan.h>
-#include "serial/serialcommands.h"
-#include "batterymonitor.h"
-#include "logging/Logger.h"
+#include <Arduino.h>
+// #include "Wire.h"
+// #include "ota.h"
+// #include "GlobalVars.h"
+// #include "globals.h"
+// #include "credentials.h"
+// #include <i2cscan.h>
+// #include "serial/serialcommands.h"
+// #include "batterymonitor.h"
+// #include "logging/Logger.h"
 
-Timer<> globalTimer;
-SlimeVR::Logging::Logger logger("SlimeVR");
-SlimeVR::Sensors::SensorManager sensorManager;
-SlimeVR::LEDManager ledManager(LED_PIN);
-SlimeVR::Status::StatusManager statusManager;
-SlimeVR::Configuration::Configuration configuration;
-SlimeVR::Network::Manager networkManager;
-SlimeVR::Network::Connection networkConnection;
+// Timer<> globalTimer;
+// SlimeVR::Logging::Logger logger("SlimeVR");
+// SlimeVR::Sensors::SensorManager sensorManager;
+// SlimeVR::LEDManager ledManager(LED_PIN);
+// SlimeVR::Status::StatusManager statusManager;
+// SlimeVR::Configuration::Configuration configuration;
+// SlimeVR::Network::Manager networkManager;
+// SlimeVR::Network::Connection networkConnection;
 
-int sensorToCalibrate = -1;
-bool blinking = false;
-unsigned long blinkStart = 0;
-unsigned long loopTime = 0;
-unsigned long lastStatePrint = 0;
-bool secondImuActive = false;
-BatteryMonitor battery;
+// int sensorToCalibrate = -1;
+// bool blinking = false;
+// unsigned long blinkStart = 0;
+// unsigned long loopTime = 0;
+// unsigned long lastStatePrint = 0;
+// bool secondImuActive = false;
+// BatteryMonitor battery;
 
 void setup()
 {
-    Serial.begin(serialBaudRate);
-    globalTimer = timer_create_default();
+//     Serial.begin(serialBaudRate);
+//     globalTimer = timer_create_default();
 
-#ifdef ESP32C3
-    // Wait for the Computer to be able to connect.
-    delay(2000);
-#endif
+// #ifdef ESP32C3
+//     // Wait for the Computer to be able to connect.
+//     delay(2000);
+// #endif
 
-    Serial.println();
-    Serial.println();
-    Serial.println();
+//     Serial.println();
+//     Serial.println();
+//     Serial.println();
 
-    logger.info("SlimeVR v" FIRMWARE_VERSION " starting up...");
+//     logger.info("SlimeVR v" FIRMWARE_VERSION " starting up...");
 
-    statusManager.setStatus(SlimeVR::Status::LOADING, true);
+//     statusManager.setStatus(SlimeVR::Status::LOADING, true);
 
-    ledManager.setup();
-    configuration.setup();
+//     ledManager.setup();
+//     configuration.setup();
 
-    SerialCommands::setUp();
+//     SerialCommands::setUp();
 
-#if IMU == IMU_MPU6500 || IMU == IMU_MPU6050 || IMU == IMU_MPU9250 || IMU == IMU_BNO055 || IMU == IMU_ICM20948 || IMU == IMU_BMI160|| IMU == IMU_ICM42688
-    I2CSCAN::clearBus(PIN_IMU_SDA, PIN_IMU_SCL); // Make sure the bus isn't stuck when resetting ESP without powering it down
-    // Fixes I2C issues for certain IMUs. Only has been tested on IMUs above. Testing advised when adding other IMUs.
-#endif
-    // join I2C bus
+// #if IMU == IMU_MPU6500 || IMU == IMU_MPU6050 || IMU == IMU_MPU9250 || IMU == IMU_BNO055 || IMU == IMU_ICM20948 || IMU == IMU_BMI160|| IMU == IMU_ICM42688
+//     I2CSCAN::clearBus(PIN_IMU_SDA, PIN_IMU_SCL); // Make sure the bus isn't stuck when resetting ESP without powering it down
+//     // Fixes I2C issues for certain IMUs. Only has been tested on IMUs above. Testing advised when adding other IMUs.
+// #endif
+//     // join I2C bus
 
-#if ESP32
-    // For some unknown reason the I2C seem to be open on ESP32-C3 by default. Let's just close it before opening it again. (The ESP32-C3 only has 1 I2C.)
-    Wire.end();
-#endif
+// #if ESP32
+//     // For some unknown reason the I2C seem to be open on ESP32-C3 by default. Let's just close it before opening it again. (The ESP32-C3 only has 1 I2C.)
+//     Wire.end();
+// #endif
 
-    // using `static_cast` here seems to be better, because there are 2 similar function signatures
-    Wire.begin(static_cast<int>(PIN_IMU_SDA), static_cast<int>(PIN_IMU_SCL));
+//     // using `static_cast` here seems to be better, because there are 2 similar function signatures
+//     Wire.begin(static_cast<int>(PIN_IMU_SDA), static_cast<int>(PIN_IMU_SCL));
 
-#ifdef ESP8266
-    Wire.setClockStretchLimit(150000L); // Default stretch limit 150mS
-#endif
-#ifdef ESP32 // Counterpart on ESP32 to ClockStretchLimit
-    Wire.setTimeOut(150);
-#endif
-    Wire.setClock(I2C_SPEED);
+// #ifdef ESP8266
+//     Wire.setClockStretchLimit(150000L); // Default stretch limit 150mS
+// #endif
+// #ifdef ESP32 // Counterpart on ESP32 to ClockStretchLimit
+//     Wire.setTimeOut(150);
+// #endif
+//     Wire.setClock(I2C_SPEED);
 
-    // Wait for IMU to boot
-    delay(500);
+//     // Wait for IMU to boot
+//     delay(500);
 
-    sensorManager.setup();
+//     sensorManager.setup();
 
-    networkManager.setup();
-    OTA::otaSetup(otaPassword);
-    battery.Setup();
+//     networkManager.setup();
+//     OTA::otaSetup(otaPassword);
+//     battery.Setup();
 
-    statusManager.setStatus(SlimeVR::Status::LOADING, false);
+//     statusManager.setStatus(SlimeVR::Status::LOADING, false);
 
-    sensorManager.postSetup();
+//     sensorManager.postSetup();
 
-    loopTime = micros();
+//     loopTime = micros();
 }
 
 void loop()
 {
-    globalTimer.tick();
-    SerialCommands::update();
-    OTA::otaUpdate();
-    networkManager.update();
-    sensorManager.update();
-    battery.Loop();
-    ledManager.update();
-#ifdef TARGET_LOOPTIME_MICROS
-    long elapsed = (micros() - loopTime);
-    if (elapsed < TARGET_LOOPTIME_MICROS)
-    {
-        long sleepus = TARGET_LOOPTIME_MICROS - elapsed - 100;//µs to sleep
-        long sleepms = sleepus / 1000;//ms to sleep
-        if(sleepms > 0) // if >= 1 ms
-        {
-            delay(sleepms); // sleep ms = save power
-            sleepus -= sleepms * 1000;
-        }
-        if (sleepus > 100)
-        {
-            delayMicroseconds(sleepus);
-        }
-    }
-    loopTime = micros();
-#endif
-    #if defined(PRINT_STATE_EVERY_MS) && PRINT_STATE_EVERY_MS > 0
-        unsigned long now = millis();
-        if(lastStatePrint + PRINT_STATE_EVERY_MS < now) {
-            lastStatePrint = now;
-            SerialCommands::printState();
-        }
-    #endif
+//     globalTimer.tick();
+//     SerialCommands::update();
+//     OTA::otaUpdate();
+//     networkManager.update();
+//     sensorManager.update();
+//     battery.Loop();
+//     ledManager.update();
+// #ifdef TARGET_LOOPTIME_MICROS
+//     long elapsed = (micros() - loopTime);
+//     if (elapsed < TARGET_LOOPTIME_MICROS)
+//     {
+//         long sleepus = TARGET_LOOPTIME_MICROS - elapsed - 100;//µs to sleep
+//         long sleepms = sleepus / 1000;//ms to sleep
+//         if(sleepms > 0) // if >= 1 ms
+//         {
+//             delay(sleepms); // sleep ms = save power
+//             sleepus -= sleepms * 1000;
+//         }
+//         if (sleepus > 100)
+//         {
+//             delayMicroseconds(sleepus);
+//         }
+//     }
+//     loopTime = micros();
+// #endif
+//     #if defined(PRINT_STATE_EVERY_MS) && PRINT_STATE_EVERY_MS > 0
+//         unsigned long now = millis();
+//         if(lastStatePrint + PRINT_STATE_EVERY_MS < now) {
+//             lastStatePrint = now;
+//             SerialCommands::printState();
+//         }
+//     #endif
 }
