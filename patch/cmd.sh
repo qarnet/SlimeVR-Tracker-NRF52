8ef8f79ff86284971272b99861cd3531b93ac925
qarnet <thomaskirschner85@yahoo.de>
2024-01-06 10:26:53 +0100

    Patch files to get correct pinout

---
+++ patch/cmd.sh
@@ -0,0 +1,9 @@
+cp -f update-feather_nrf52840_express_bootloader-0.8.0_nosd.uf2 ~/.platformio/packages/framework-arduinoadafruitnrf52/bootloader/feather_nrf52840_express/update-feather_nrf52840_express_bootloader-0.8.0_nosd.uf2
+
+cp -f feather_nrf52840_express_bootloader-0.8.0_s140_6.1.1.zip ~/.platformio/packages/framework-arduinoadafruitnrf52/bootloader/feather_nrf52840_express/feather_nrf52840_express_bootloader-0.8.0_s140_6.1.1.zip
+
+cp -f feather_nrf52840_express_bootloader-0.8.0_s140_6.1.1.hex ~/.platformio/packages/framework-arduinoadafruitnrf52/bootloader/feather_nrf52840_express/feather_nrf52840_express_bootloader-0.8.0_s140_6.1.1.hex
+
+cp -f variant.h ~/.platformio/packages/framework-arduinoadafruitnrf52/variants/feather_nrf52840_express/variant.h
+
+cp -f variant.cpp ~/.platformio/packages/framework-arduinoadafruitnrf52/variants/feather_nrf52840_express/variant.cpp
